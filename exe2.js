103cad106b46b4e56a994ab76c7820143d2bc407
Jefersonsilvadev <jeferson.s.s@live.com>
2023-03-07 20:59:24 -0300

    novos exercícios

--- exe2.js
+++ exe2.js
@@ -16,12 +16,25 @@ function exe1()
 function exe2()
 {
     var resultado = document.getElementById("resultado");
-    var texto = document.getElementById("texto");
+    var texto = document.getElementById("texto").value;
 
-    var invertida = reverse(texto.value);
+    var invertido = "";
+    var tam = texto.length - 1;
 
-    resultado.innerHTML = invertida;
-    texto.value = "";
+
+    while (tam >= 0)
+    {
+        invertido += texto[tam];
+        tam--;
+    }
+
+    //for (var tam = texto.length -1; tam >= 0; tam--)
+    //{
+    //    invertido += texto[tam];
+    //}
+
+    resultado.innerHTML = invertido;
+    //texto.value = "";
 }
 
 // oculta o cpf
@@ -37,4 +50,19 @@ function exe3 ()
 
     resultado.innerHTML = inicio + ".***.***." + fim;
     texto.value = "";
+}
+
+function exe4 ()
+{
+    var resultado = document.getElementById("resultado");
+    var texto = document.getElementById("texto");
+
+    // transforma o texto em letra minúscula
+    var url = texto.value.toLowerCase();
+
+    url = url.split(" ");
+    resultado.innerHTML = url.join("_");
+
+    //resultado.innerHTML = url.replace(/ /g , "_");
+    
 }
